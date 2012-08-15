23076b1a4b3120c21163e5a6cd7968c5de26cbd2
Duarte Barbosa <duarte.barbosa@gmail.com>
2012-08-15 21:12:41 +0100

    duplicate graph bug fixed; dynamic loading of every graph; minor bugfixes

--- visual.js
+++ visual.js
@@ -19,68 +19,31 @@ function show(obj){
 		contentShown = obj;
 
 		if(contentShown == '#sysmology'){
-			
 			sysmologyPresentTooltips = $("#sysmologyPresentTooltips").is(':checked');
 			sysmologyShowEffects = $("#sysmologyShowEffects").is(':checked');
 		
 			for (counter = 1, content = '#sysmologyNode1'; counter <= 13; counter++, content = '#sysmologyNode' + counter){
+				removePanel('#sysmologyPanelNode' + counter);
 				if ($(content).is(':checked')){
-					$('#sysmologyPanelNode' + counter).show();//removeAttr("display");
+					addPanel(counter);
 					drawSysmology(counter);
 				}
-				else
-					$('#sysmologyPanelNode' + counter).hide();//css("display", "none");
 			}
 		}
 		else if(contentShown == '#network'){
-
 			networkPresentTooltips = $("#networkPresentTooltips").is(':checked');
 			networkShowEffects = $("#networkShowEffects").is(':checked');
 		
 			for (counter = 1, content = '#networkNode1'; counter <= 13; counter++, content = '#networkNode' + counter){
+				removePanel('#networkPanelNode' + counter);
 				if ($(content).is(':checked')){
-					$('#networkPanelNode' + counter).show();//css("display", "block");
+					addPanel(counter);
 					drawNetwork(counter);
 				}
-				else
-					$('#networkPanelNode' + counter).hide();//css("display", "none");
 			}
 		}
 	}
 }
-
-
-function setupPortal(portal) {
-
-	//vir cá fora buscar o valor - doesn't matter which portal it is as they are equal!
-	var width = $(networkViewPortal).width() - 20;
-	var height = $(networkViewPortal).height();
-
-	var counter;
-	var tmp;
-
-	var aux = portal + "PanelNode";
-
-	for (counter = 1; counter <= 13; counter++){
-		tmp = aux + counter;
-		$(tmp).width(width);
-		$(tmp).height(height*0.9);
-
-	}
-
-	aux = portal + "GraphNode";
-
-	for (counter = 1; counter <= 13; counter++){
-		tmp = aux + counter;
-		$(tmp).width(width);
-		$(tmp).height(height*0.8);
-
-	}
-	
-	$(portal + "ViewPortal").portal('resize');
-
-}
-
 
 function visualTweaker() {
 
@@ -105,9 +68,6 @@ function visualTweaker() {
 		fit:true
 	});
 
-	setupPortal("#network");
-	setupPortal("#sysmology");
-
 	hide('#config_network');
 	hide('#config_sysmology');
 	hide('#sysmology');
@@ -115,6 +75,54 @@ function visualTweaker() {
 	show('#welcome');
 
 }
+
+// panel stuff
+
+function collapsePanel(panel){
+	$(panel).panel('collapse');
+}
+
+function removePanel(panel){
+	$(contentShown + 'ViewPortal').portal('remove', $(panel));
+	$(contentShown + 'ViewPortal').portal('resize');
+}
+		
+function addPanel(node){
+
+	//vir cá fora buscar o valor - doesn't matter which portal it is as they are equal!
+	var width = $(networkViewPortal).width() - 20;
+	var height = $(networkViewPortal).height();
+	
+	var p = $('<div/>').appendTo('body');
+	if(contentShown == "#sysmology")
+		p.panel({
+			id: 'sysmologyPanelNode' + node,
+			title: 'Node ' + node,
+			content:'<div id="sysmologyGraphNode' + node + '" width="' + (width - 20) + '" height="' + (height*0.8) +'">[Something wrong regarding Highcharts lib]</div><br/>',
+			height:(height*0.9),
+			closable:false,
+			collapsible:true,
+			style: 'padding:10px'
+		});
+	else
+		p.panel({
+			id: 'networkPanelNode' + node,
+			title: 'Node ' + node,
+			content:'<canvas id="networkGraphNode' + node + '" width="' + (width - 20) + '" height="' + (height*0.8) +'">[No canvas support]</canvas><br/>',
+			height:(height*0.9),
+			closable:false,
+			collapsible:true,
+			style: 'padding:10px'
+		});
+			
+	$(contentShown + 'ViewPortal').portal('add', {
+		panel:p,
+		columnIndex:0
+	});
+
+	$(contentShown + 'ViewPortal').portal('resize');
+}
+
 
 //spinner stuff
 
